66b5547439e41d8d5301c2593652185ee4b5b70b
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-03-16 10:29:18 -0300

    Criando telas da screen tab

---
+++ src/screens/Tab/Notification/Notification.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export const Notification = () => {
+    return (
+        <Container>
+            <Title>Notification</Title>
+        </Container>
+    )
+}
---
+++ src/screens/Tab/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export const Profile = () => {
+    return (
+        <Container>
+            <Title>Profile</Title>
+        </Container>
+    )
+}
